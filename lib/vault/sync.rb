2bb50535eb11bc938d7fc4a57dceb0054f923dc5
Jeff Byrnes <thejeffbyrnes@gmail.com>
2021-02-10 14:29:31 -0500

    Initial commit
    Generated via:
    
        bundle gem vault-sync

---
+++ lib/vault/sync.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "sync/version"
+
+module Vault
+  module Sync
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/vault/sync_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Vault::Sync do
+  it "has a version number" do
+    expect(Vault::Sync::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
