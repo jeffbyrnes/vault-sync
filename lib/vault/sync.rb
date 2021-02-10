35753781198f728a4c3a549f002298d1e8f55ba9
Jeff Byrnes <thejeffbyrnes@gmail.com>
2021-02-10 14:54:39 -0500

    chore: Clean up Ruby per Rubocop

--- lib/vault/sync.rb
+++ lib/vault/sync.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "sync/version"
+require_relative 'sync/version'
 
 module Vault
   module Sync
--- lib/vault/sync/version.rb
+++ lib/vault/sync/version.rb
@@ -2,6 +2,6 @@
 
 module Vault
   module Sync
-    VERSION = "0.1.0"
+    VERSION = '0.1.0'
   end
 end
